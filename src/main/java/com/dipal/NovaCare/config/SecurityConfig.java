f8e1940f8000443c528743954b296a0a6d127df9
Dipal Das <dasdipal8888@gmail.com>
2025-08-30 22:47:02 +0530

    enabled cors in securityconfig

--- src/main/java/com/dipal/NovaCare/config/SecurityConfig.java
+++ src/main/java/com/dipal/NovaCare/config/SecurityConfig.java
@@ -24,6 +24,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
 
